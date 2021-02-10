87efc5b3da5e27c176a90fc8d6048746822905c3
David Mutitu <37056910+dyrell@users.noreply.github.com>
2021-02-10 13:35:06 +0800

    Update and rename CheckPrimes to CheckPrimes.cpp
    File to check if number is prime and if not get prime factors of the number

---
+++ CheckPrimes.cpp
@@ -0,0 +1,102 @@
+
+#include <iostream>
+#include <type_traits>
+#include <vector>
+#include <numeric>
+#include <functional> 
+#include <algorithm>
+
+typedef struct Prime
+{
+    template<class X> requires std::is_integral<X>::value    
+    bool constexpr operator()(X z) const
+     {
+        bool lclBl = true;        
+        for(X x=2; x<=(z/2); x++)
+        {
+           if(z%x == 0)
+           {
+               lclBl = false;
+               break;
+           }
+           else
+               continue;
+         }
+        if (z==1)
+            lclBl = false;
+        return lclBl;
+    };
+ }IsPrime;
+
+typedef struct PrimeFaq
+{
+    template<class X> requires std::is_integral<X>::value    
+   std::vector<X> constexpr operator()(X z) const
+     {
+        X vsize = z;
+        IsPrime lclprime;
+        std::vector<X> lclVec; 
+        X resz;
+                 
+       if(lclprime(z)==true)
+       {
+         lclVec.emplace_back(1);
+         lclVec.emplace_back(z);
+       }
+       else
+       {        
+        for(X x=2; x*x<=(vsize); x++)
+        {
+           while(vsize%x == 0) 
+           {   
+             if (lclprime(x)==true)
+               {
+                 resz = std::accumulate(std::begin(lclVec), std::end(lclVec), 1, std::multiplies<X>());                
+                if(resz < z) 
+                 {   
+                  lclVec.emplace_back(x);
+                  }   
+                  vsize /= x;                  
+               }
+               if (lclprime(vsize)==true)
+               {
+                 resz = std::accumulate(std::begin(lclVec), std::end(lclVec), 1, std::multiplies<X>());                
+                 if (resz < z)
+                 { 
+                  lclVec.emplace_back(vsize);
+                 }   
+               }               
+             }                     
+           }
+          }
+          return lclVec;
+           }        
+       }PrimeFactors;
+
+int main()
+{
+    
+   PrimeFactors lclPrmz;
+   IsPrime checkPrime;
+        
+    long int q = 70577633;
+    std::vector<long int> mainVec;
+    
+     std::function<long int(long int)>funcPrime = checkPrime;
+    
+    if (funcPrime(q))
+        std::cout<<"The number: "<<q<<" is prime\n\n";
+    else
+        std::cout<<"The number: "<<q<<" ain't prime\n\n";
+
+    mainVec = lclPrmz(q);
+
+   std::cout<<"The prime factors of: "<<q<<" are:\n\n";   
+   std::string delim = "";
+   for(auto x : mainVec)
+    {
+         std::cout<<delim<<x;
+         delim = ",\n";
+    }
+}
+
