2595bf214edab34e7a5126db7d7d4b9bd44fd747
David Mutitu <37056910+dyrell@users.noreply.github.com>
2021-02-12 13:34:32 +0800

    CheckPrimes.cpp
    Bug fix.

--- CheckPrimes.cpp
+++ CheckPrimes.cpp
@@ -12,11 +12,11 @@ typedef struct Prime
     bool operator()(X z) const
      {
         bool lclBl = true;
-        if (z <=1 )
+        if ((z <= 1 )||(z%2 == 0))
         {
            lclBl = false;
         }
-       if (z == 2 )
+       if (z == 2)
           {
            lclBl = true;
           }
@@ -88,7 +88,7 @@ int main()
    PrimeFactors lclPrmz;
    IsPrime checkPrime;
         
-    long int q = 844379265313;
+    long int q = 16;
     std::vector<long int> mainVec;
     
      std::function<long int(long int)>funcPrime = checkPrime;
