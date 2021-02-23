3a25ef67adf429954163196bed6c409e6c300bef
David Mutitu <37056910+dyrell@users.noreply.github.com>
2021-02-23 15:16:12 +0800

    PerfectForwardIntoVec.cpp
    Added pointer representation.

--- PerfectForwardIntoVec.cpp
+++ PerfectForwardIntoVec.cpp
@@ -1,4 +1,5 @@
 // This file is a "Insert values" in C++ language by GCC for wandbox.
+// This file is a "Insert values" in C++ language by GCC for wandbox.
 #include <iostream>
 #include <vector>
 
@@ -23,16 +24,48 @@ template<class T>
         std::cout<<z<<"\n";
    }
   };  
+//pointer Specialisation
+template<class T>
+  class Compact<T*>
+  {
+   private:
+    std::vector<T*> vecVals;
+   public:
+      Compact()=default;
+    
+   template<typename... U>
+    void insertFunc(U*&&... v)
+      {
+       auto pushVals = [&](auto&& x){ vecVals.push_back(std::forward<decltype(x)>(x));};
+      (..., pushVals(std::forward<U*>(v)));
+   }
+      
+   void printOut()
+   {
+     for(auto z : vecVals)
+        std::cout<<*z<<"\n";
+   }
+  };  
+
+
+
 
 int main()
 {
     std::cout << "Hello, time to insert!\n" << std::endl;
     Compact<int> mainComp;
+    Compact<int*> ptrComp;
     
-    int a = -1, b = -2, c = -3, d = -4, e = -5, f= -6, g = -7, h = -8, i = -9;
+   int a = -1, b = -2, c = -3, d = -4, e = -5, f= -6, g = -7, h = -8, i = -9;
     
     mainComp.insertFunc(0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i);
+    ptrComp.insertFunc(&a,&b,&c,&d,&e,&f,&g,&h,&i);
+    
     mainComp.printOut(); 
+    std::cout << "\n\nHello, insert from pointer!\n" << std::endl;
+    ptrComp.printOut(); 
+    
     return 0;
 }
+
 
