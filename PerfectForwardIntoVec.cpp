d4c3e0462e18ef5d5cbe94ec2f9ee64e7a95c6dc
David Mutitu <37056910+dyrell@users.noreply.github.com>
2021-02-26 11:54:45 +0800

    Update PerfectForwardIntoVec.cpp
    Added the transform to show multiplication

--- PerfectForwardIntoVec.cpp
+++ PerfectForwardIntoVec.cpp
@@ -1,6 +1,7 @@
 // This file is a "Insert values" in C++ language by GCC for wandbox.
-// This file is a "Insert values" in C++ language by GCC for wandbox.
 #include <iostream>
+#include <algorithm>
+#include <functional>
 #include <vector>
 
 template<class T>
@@ -16,8 +17,21 @@ template<class T>
       {
        auto pushVals = [&](auto&& x){ vecVals.push_back(std::forward<decltype(x)>(x));};
       (..., pushVals(std::forward<U>(v)));
-   }
-      
+      }
+  
+ std::vector<T> getCompactVect(void)
+    {
+     std::vector<T> lclVec;
+    int vSize = vecVals.size();
+      std::cout << "What is the size:  " <<vecVals.size()<<std::endl;
+     for(int x=0; x < vSize; x++)
+     {
+        // std::cout << "What is the vecVals[x]:  " <<vecVals[x]<<std::endl;
+         lclVec.push_back(vecVals[x]);         
+      }
+        return(lclVec);
+     }   
+           
    void printOut()
    {
      for(auto z : vecVals)
@@ -39,7 +53,20 @@ template<class T>
        auto pushVals = [&](auto&& x){ vecVals.push_back(std::forward<decltype(x)>(x));};
       (..., pushVals(std::forward<U*>(v)));
    }
-      
+   
+std::vector<T> getCompactVect(void)
+    {
+     std::vector<T> lclVec;
+     int vSize = vecVals.size();;
+     
+     for(int x=0; x<vSize; x++)
+     {
+       // std::cout << "What is the *vecVals[x]:  " <<*vecVals[x]<<std::endl;
+        lclVec.push_back(*vecVals[x]);      
+      }
+        return(lclVec);
+     } 
+     
    void printOut()
    {
      for(auto z : vecVals)
@@ -47,25 +74,44 @@ template<class T>
    }
   };  
 
-
-
-
 int main()
 {
     std::cout << "Hello, time to insert!\n" << std::endl;
     Compact<int> mainComp;
     Compact<int*> ptrComp;
+    std::vector<int> finalCompVect;
     
    int a = -1, b = -2, c = -3, d = -4, e = -5, f= -6, g = -7, h = -8, i = -9;
     
-    mainComp.insertFunc(0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i);
+    mainComp.insertFunc(a,b,c,d,e,f,g,h,i);
     ptrComp.insertFunc(&a,&b,&c,&d,&e,&f,&g,&h,&i);
     
     mainComp.printOut(); 
     std::cout << "\n\nHello, insert from pointer!\n" << std::endl;
     ptrComp.printOut(); 
     
+    std::vector<int> mainCompVect = mainComp.getCompactVect();
+    std::vector<int> ptrCompVect =  ptrComp.getCompactVect();    
+    
+     std::cout << "\n\nOutput contents 1!\n" << std::endl; 
+    for(auto z : mainCompVect)
+    {
+        std::cout<<z<<"\n";
+    }
+    
+     std::cout << "\n\nOutput contents 2!\n" << std::endl;
+     for(auto z : ptrCompVect)
+     {
+        std::cout<<z<<"\n";
+     }
+    finalCompVect.resize(ptrCompVect.size());
+    std::transform(mainCompVect.begin(), mainCompVect.end(), ptrCompVect.begin(), finalCompVect.begin(), std::multiplies<int>());
+    
+     std::cout << "\n\nDot product of both vectors is \n" << std::endl;
+     for(auto z : finalCompVect)
+     {
+        std::cout<<z<<"\n";
+     }
+    
     return 0;
 }
-
-
