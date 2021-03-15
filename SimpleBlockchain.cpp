0ae60a760f9cbc20d82e7fb915066dc492ff5851
David Mutitu <37056910+dyrell@users.noreply.github.com>
2021-03-15 16:31:15 +0800

    Update SimpleBlockchain.cpp
    Added different method to update

--- SimpleBlockchain.cpp
+++ SimpleBlockchain.cpp
@@ -1,6 +1,6 @@
 // This file is a simple demonstration on how blockchain works
 #include <iostream>
-#include <cstdlib>
+#include <cassert>
 #include <string>
 #include <memory>
 
@@ -15,43 +15,36 @@ class Block
                 std::hash<std::string> cnstrctHsh;
                 hashTrans = cnstrctHsh(transaction);
                }
-            Block(const std::string& trans, const Block& blck) : transaction(trans) 
+           Block(const std::string& trans, const Block& blck) : transaction(trans) 
                {
                 std::hash<std::string> cnstrctHsh;
                 hashTrans = (cnstrctHsh(transaction))^blck.getHash();
                }
-            std::size_t getHash() const
+           std::size_t getHash() const
               {
-                return(hashTrans);
+                return(this->hashTrans);
               }
-            void printHash(void) 
+          void printHash(void) 
              {
                 std::cout<<"The hash number is: "<<hashTrans<<std::endl;
              }
        };
 
-
 int main()
 {
     std::cout << "Hello, Wandbox time to blockchain" << std::endl;
     
-    std::string trans0 {"blah0"};
-    std::string trans1 {"blah1"};
-    std::string trans2 {"blah2"};
-    std::string trans3 {"blah3"};
+    std::string trans0 {"block0"};
+    std::string trans1 {"block1"};
+    std::string trans2 {"block2"};
+    std::string trans3 {"block3"};
+           
+    std::unique_ptr<Block> uPtr0 = std::make_unique<Block>(trans0);
+    std::unique_ptr<Block> uPtr1 = std::make_unique<Block>(trans1, *uPtr0) ;
+    std::unique_ptr<Block> uPtr2 = std::make_unique<Block>(trans2, *uPtr1);
+    std::unique_ptr<Block> uPtr3 = std::make_unique<Block>(trans3, *uPtr2);
     
-   Block *gen = new Block(trans0);
-   Block *blck1 = new Block(trans1, *gen); 
-   Block *blck2 = new Block(trans2, *blck1); 
-   Block *blck3 = new Block(trans3, *blck2);
-    
-   (*blck3).printHash();
+   (*uPtr3).printHash();
    
-   delete gen;
-   delete blck1;
-   delete blck2;
-   delete blck3;
      
 }
-
-
