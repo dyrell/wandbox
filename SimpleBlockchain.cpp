822ed29c0ae72cbc5da448b57712f918e9b0aeaa
David Mutitu <37056910+dyrell@users.noreply.github.com>
2021-03-16 16:12:16 +0800

    Update SimpleBlockchain.cpp
    Update to use unique_ptr of pointer

--- SimpleBlockchain.cpp
+++ SimpleBlockchain.cpp
@@ -1,5 +1,6 @@
 // This file is a simple demonstration on how blockchain works
 #include <iostream>
+#include <cstdlib>
 #include <cassert>
 #include <string>
 #include <memory>
@@ -10,25 +11,43 @@ class Block
             std::string transaction;
             std::size_t hashTrans;
         public:
+            Block() 
+               {
+                std::hash<std::string> cnstrctHsh;
+                hashTrans = cnstrctHsh("hello");
+               }
             Block(const std::string& trans) : transaction(trans) 
                {
                 std::hash<std::string> cnstrctHsh;
                 hashTrans = cnstrctHsh(transaction);
                }
-           Block(const std::string& trans, const Block& blck) : transaction(trans) 
+          Block(const std::string& trans, const Block& blck) : transaction(trans) 
                {
                 std::hash<std::string> cnstrctHsh;
                 hashTrans = (cnstrctHsh(transaction))^blck.getHash();
+               }  
+         Block(const Block* & blck)
+               {
+                std::cout<<"Copy constructor called "<<std::endl;
+                this->transaction = blck->getTransact();
+                std::hash<std::string> cnstrctHsh;
+                hashTrans = (cnstrctHsh(transaction))^blck->getHash();
                }
-           std::size_t getHash() const
-              {
+          std::size_t getHash() const
+              {                
                 return(this->hashTrans);
               }
+          std::string getTransact() const
+              {
+                return(this->transaction); 
+               }
           void printHash(void) 
              {
-                std::cout<<"The hash number is: "<<hashTrans<<std::endl;
+                std::cout<<"The Transaction is: "<<transaction<<std::endl;
+                std::cout<<"The Hash number is: "<<hashTrans<<std::endl;
              }
        };
+
 
 int main()
 {
@@ -43,8 +62,10 @@ int main()
     std::unique_ptr<Block> uPtr1 = std::make_unique<Block>(trans1, *uPtr0) ;
     std::unique_ptr<Block> uPtr2 = std::make_unique<Block>(trans2, *uPtr1);
     std::unique_ptr<Block> uPtr3 = std::make_unique<Block>(trans3, *uPtr2);
-    
-   (*uPtr3).printHash();
+    std::unique_ptr<Block*> uPtr4 = std::make_unique<Block*>(uPtr3.release());
+           
+    std::cout<<*uPtr4<<std::endl;
+    if(*uPtr4 != 0)
+        (*(*uPtr4)).printHash();
    
-     
 }
